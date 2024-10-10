b8b8356b9906e4bfc8dbba26ca388d2e6e4b6151
Taimoor Fazal <77178021+taimoor-fazal@users.noreply.github.com>
2024-10-10 09:23:27 +0200

    Make the code ready for the backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Data } from "./list/list";
+import { Main } from "./main/main";
 
 function App() {
   return (
     <div>
-      <Data />
+      {/* <Data /> */}
+      <Main />
     </div>
   );
 }
--- src/main/main.tsx
+++ src/main/main.tsx
@@ -1,51 +1,68 @@
-import React, { ReactNode, useContext, useEffect, useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 
 import CSS from "./main.module.css";
 
 const initialState = {
   count: 0,
-  tempCount: 10,
   setCount: (count: number) => {},
 };
 
+const initialStateContext2 = {
+  count2: 10,
+  setCount2: (count: number) => {},
+};
+
 const AppContext = React.createContext(initialState);
+const AppContext2 = React.createContext(initialStateContext2);
 
 export const AppProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
   const [count, setCount] = useState(0);
-  const [tempCount] = useState(10);
 
   return (
-    <AppContext.Provider value={{ count, setCount, tempCount }}>
+    <AppContext.Provider value={{ count, setCount }}>
       {children}
     </AppContext.Provider>
   );
 };
 
-export const Main: React.FC = () => {
+export const AppProvider2: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [count2, setCount2] = useState(10);
+
   return (
-    <AppProvider>
-      <Wrapper>
-        <ExpensiveComponent />
-      </Wrapper>
-    </AppProvider>
+    <AppContext2.Provider value={{ count2, setCount2 }}>
+      {children}
+    </AppContext2.Provider>
   );
 };
 
-const Wrapper: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const { count, setCount } = useContext(AppContext);
+export const Main: React.FC = () => {
+  return <Wrapper />;
+};
+
+//const Wrapper: React.FC<{ children: ReactNode }> = ({ children }) => {
+const Wrapper: React.FC = () => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("Wrapper mounted");
+  }, []);
+  useEffect(() => {
+    console.log("Wrapper rendered");
+  });
+
   return (
     <div className={CSS.body}>
       <h1>Count: {count}</h1>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
-      {children}
-      {/* <ExpensiveComponent count={count} /> */}
+      <ExpensiveComponent />
     </div>
   );
 };
-const ExpensiveComponent: React.FC = () => {
+const ExpensiveComponent = () => {
   useEffect(() => {
     console.log("ExpensiveComponent mounted");
   }, []);
@@ -54,20 +71,26 @@ const ExpensiveComponent: React.FC = () => {
     console.log("ExpensiveComponent rendered");
   });
 
-  // for (let i = 0; i < 1000; i++) {
-  //   console.log(i);
-  // }
-
   return (
     <div>
       {"ExpensiveComponent"}
       <SecondExpensiveComponent />
     </div>
   );
+
+  // const { count2, setCount2 } = useContext(AppContext2);
+
+  // return (
+  //   <div className={CSS.body}>
+  //     <h1>Count: {count2}</h1>
+  //     <button onClick={() => setCount2(count2 + 1)}>Increment</button>
+  //     <button onClick={() => setCount2(count2 - 1)}>Decrement</button>
+  //     <SecondExpensiveComponent />
+  //   </div>
+  // );
 };
 
-const SecondExpensiveComponent: React.FC = () => {
-  const { setCount } = useContext(AppContext);
+const SecondExpensiveComponent = () => {
   useEffect(() => {
     console.log("SecondExpensiveComponent mounted");
   }, []);
@@ -75,10 +98,6 @@ const SecondExpensiveComponent: React.FC = () => {
   useEffect(() => {
     console.log("SecondExpensiveComponent rendered");
   });
-
-  // for (let i = 0; i < 1000; i++) {
-  //   console.log("Second: " + i);
-  // }
 
   return <div>{"SecondExpensiveComponent"}</div>;
 };
